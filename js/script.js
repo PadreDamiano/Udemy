4c536f1bd5241df578a9bbfae1b2fdb617c63333
Vitaliy Demyanenko <padre.vitaliy@gmail.com>
2022-07-03 17:04:52 +0300

    Practik 4

--- js/script.js
+++ js/script.js
@@ -1,72 +1,73 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start ();
-
 let personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const film = prompt('Один из последних просмотренных фильмов?', ''),
+                  count = +prompt('На сколько оцените его?', '');
+        
+            if (film != "" && count != "" && film != null  && count != null && film.length < 50){
+                personalMovieDB.movies[film] = count;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;        
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        }else if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    showMyDB: function(hiden) {
+        if (!hiden) {
+            console.log(personalMovieDB);
+        }
+    },    
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat){
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 0; i < 3; i++){
+            let genre = prompt(`Ваш любимый жанр под номером ${i+1}`, '');
+            if (genre == '' || genre == null){
+                i--;
+            } else {
+                personalMovieDB.genres[i]= genre;
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
+    }
 };
 
-function rememberMyFilms(){
-    for (let i = 0; i < 2; i++) {
-        const film = prompt('Один из последних просмотренных фильмов?', ''),
-              count = +prompt('На сколько оцените его?', '');
-    
-        if (film != "" && count != "" && film != null  && count != null && film.length < 50){
-            personalMovieDB.movies[film] = count;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;        
-        }
-    }
-}
 
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Вы классический зритель");
-    }else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden.privat) {
-        console.log(hidden);
-    }
-}
-
-function writeYourGenres(){
-    for (let i = 0; i < 3; i++){
-        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`, '');
-    }
-}
-
-writeYourGenres();
-showMyDB(personalMovieDB);
-    /* Задание на урок:
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
+/*    
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
